186c79ef22b4c297ecd242316018e91c02f4a62c
owolabimykel-glitch <owolabimykel@gmail.com>
2026-05-04 14:49:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const button = document.querySelector("#helloButton");
 const message = document.querySelector("#message");
 
 button.addEventListener("click", () => {
-  message.textContent = "Thanks for visiting my website!";
+  message.textContent = "Thanks for reaching out to Toby Web Studio. I will reply as soon as I can.";
 });
